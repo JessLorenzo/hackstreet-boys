1fbe4bbe1cd4aeb31194df69fb4e7a5a4e5d6afd
Jessica Lorenzo <jessicalorenzo32@gmail.com>
2025-03-28 00:38:02 -0400

    create game modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container">
+      <div className="container__modal" style={{}}>
+        <h1>What is your favorite movie</h1>
+        <div className="container__buttons">
+          <button>choice 1</button>
+          <button>choice 2</button>
+          <button>choice 3</button>
+          <button>choice 4</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
