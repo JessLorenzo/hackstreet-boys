95863529dceeeb86bf82843d5c8afef03a02b1b7
Jessica Lorenzo <jessicalorenzo32@gmail.com>
2025-03-28 11:36:21 -0400

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import "./App.scss";
-import HackstreetButton from "./components/HackstreetButton/HackstreetButton.jsx";
 
 function App() {
-  return (
-    <div className="container">
-      <div className="container__modal">
-        <h1>What is your favorite movie</h1>
-        <div className="container__buttons">
-          <HackstreetButton>choice 1</HackstreetButton>
-          <HackstreetButton>choice 2</HackstreetButton>
-          <HackstreetButton>choice 3</HackstreetButton>
-          <HackstreetButton>choice 4</HackstreetButton>
-        </div>
-      </div>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ src/pages/TriviaQuestions/TriviaQuestion.jsx
@@ -0,0 +1,20 @@
+import "./TriviaQuestion.scss";
+import HackstreetButton from "./components/HackstreetButton/HackstreetButton.jsx";
+
+function TriviaQuestion() {
+  return (
+    <div className="container">
+      <div className="container__modal">
+        <h1>Guess this movie:</h1>
+        <div className="container__buttons">
+          <HackstreetButton>choice 1</HackstreetButton>
+          <HackstreetButton>choice 2</HackstreetButton>
+          <HackstreetButton>choice 3</HackstreetButton>
+          <HackstreetButton>choice 4</HackstreetButton>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TriviaQuestion;
