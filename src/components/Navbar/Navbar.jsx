1edabc97f30e8e4bd1476e91e05206f798b765e5
Jessica Lorenzo <jessicalorenzo32@gmail.com>
2025-03-28 13:28:38 -0400

    stuff

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import "./Navbar.scss";
+import Logo from "../../assets/icons/icons.svg";
 
 function Navbar() {
   return (
     <div className="navbar">
-      <img src="src/assets/icons/icons.svg" alt="max logo" />
+      <img src={Logo} alt="max logo" />
       <span className="navbar__text">TRIVIA</span>
     </div>
   );
